b9757c67f01803e63658f518cda1f5811fb7056c
Nir Kopler <nirkopler@gmail.com>
2022-07-07 23:48:05 +0300

    minor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,18 +8,10 @@ export const Home: React.FC = observer(() => {
             <header className='App-header'>
                 <img src={logo} className='App-logo' alt='logo' />
                 <p>react-typescript-mobx-router</p>
-                <p>my favorite client setup</p>
                 <p>
                     MobX test {':)'} {mainStore.count}
                 </p>
                 <button onClick={() => mainStore.countUp()}>count ++</button>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    Learn React
-                </a>
             </header>
         </div>
     );
